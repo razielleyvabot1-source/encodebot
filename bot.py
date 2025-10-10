484809f04f3b724dd6aa75f1f2cad263b3ae3f1e
razielv2encode <razielleyvabot1@gmail.com>
2025-10-10 15:02:29 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -1842,7 +1842,6 @@ async def compress_video(client, message: Message, start_msg):
             '-r', user_video_settings['fps'],
             '-preset', user_video_settings['preset'],
             '-c:v', user_video_settings['codec'],
-            '-threads', '1',
             compressed_video_path
         ]
         logger.info(f"Comando FFmpeg: {' '.join(ffmpeg_command)}")
