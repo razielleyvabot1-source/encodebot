d5c73d02b437d9006d7202dfd646349ec4c49518
razielv2encode <razielleyvabot1@gmail.com>
2025-10-12 12:01:58 -0400

    config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from logging import WARNING
 import os
 
 
-API_ID = os.environ.get("API_ID","28193212")
-API_HASH = os.environ.get( "API_HASH","14c5ec97b18a391d526e4a461e4a5f82")
+API_ID = os.environ.get("API_ID","23810582")
+API_HASH = os.environ.get( "API_HASH","079750c767b2d4154acfff724a1a6b6e")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8049618344:AAHufMuSrA7kgvOIX1PpuXP6FAWqm793si4") 
 ADMINS_IDS = [int(x) for x in os.environ.get("ADMINS", "5644237743,6586671402").split(",") if x]
 USERS = [int(x) for x in os.environ.get("USERS", "").split(",") if x]
